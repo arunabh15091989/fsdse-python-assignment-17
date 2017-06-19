404421d0e206f6a2eae4c0d55e4d492c64994ff9
arunabh15091989 <Arunabh.social@gmail.com>
2017-06-19 07:23:54 +0000

    Done

--- build.py
+++ build.py
@@ -5,10 +5,10 @@ def solution_asc(dic):
     '''
     Enter your code here
     '''
-
-
+    # return sorted(dic.items(), reverse=True, key= lambda x:x[1])
+    return sorted(dic.items())
 def solution_desc(dic):
     '''
     Enter your code here
     '''
-
+    return sorted(dic.items(), reverse = True)
